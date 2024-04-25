4e671ace8d3b7c16f37cdb82171be07e5d118716
Doreen Luo <142925283+DoreenLoL@users.noreply.github.com>
2024-04-25 15:05:48 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/app-class-pwa/sw.js', { scope: '/app-class-pwa/' })})}
